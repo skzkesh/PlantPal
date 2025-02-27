78dd1c088cbb554e7bfa06627686fd36fb2bf1aa
Keshia Poedjiono <keshiapud@gmail.com>
2025-02-27 01:30:00 -0400

    Created CredentialValidator class, closes #8

---
+++ app/src/androidTest/java/com/example/plantpal/validator/CredentialValidator.kt
@@ -0,0 +1,43 @@
+package com.example.plantpal.validator
+
+class CredentialValidator {
+
+    fun isEmptyEmailAddress(emailAddress: String): Boolean {
+        return emailAddress.trim { it <= ' ' }.isEmpty()
+    }
+
+    fun isValidEmailAddress(emailAddress: String): Boolean {
+        val atSymbol = emailAddress.indexOf('@')
+        val emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"
+
+        if (atSymbol > 0 && atSymbol < emailAddress.length - 1 && emailAddress.matches(emailRegex.toRegex())) {
+            return true
+        }
+
+        return false
+    }
+
+    fun isValidPassword(password: String): Boolean {
+        var specialCharCount = 0
+        var alphanumericalCount = 0
+
+
+        for (c in password.toCharArray()) {
+            if (c == '!' || c == '@' || c == '#') {
+                specialCharCount += 1
+            } else if (Character.isLetterOrDigit(c)) {
+                alphanumericalCount += 1
+            }
+        }
+
+        if (specialCharCount == 2 && alphanumericalCount == 7) {
+            return true
+        }
+
+        return false
+    }
+
+    fun isValidRole(role: String): Boolean {
+        return role != "Select your role"
+    }
+}
