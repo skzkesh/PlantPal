02b80dd4fae7bc8685d528378b76f9ccfad3e3ef
Keshia Poedjiono <keshiapud@gmail.com>
2025-02-27 02:27:22 -0400

    initialize registration page views and intent, closes #6

--- app/src/main/java/ui/screens/RegistrationPage.kt
+++ app/src/main/java/ui/screens/RegistrationPage.kt
@@ -1,4 +1,60 @@
 package ui.screens
 
-class RegistrationPage {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.plantpal.R
+
+class RegistrationPage : AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var emailAddress: Button
+    private lateinit var password: EditText
+    private lateinit var errorText: TextView
+    private lateinit var registerButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_registrationpage)
+
+        initializeViews()
+
+        registerButton.setOnClickListener {
+            moveToDashboardPage()
+        }
+    }
+
+    fun initializeViews(){
+        username = findViewById(R.id.registrationEditTextUsername)
+        emailAddress = findViewById(R.id.registrationEditTextEmailAddress)
+        password = findViewById(R.id.registrationEditTextPassword)
+        errorText = findViewById(R.id.registrationErrorText)
+        registerButton = findViewById(R.id.registrationRegisterButton)
+    }
+
+    fun getUsername(): String {
+        return username.text.toString().trim { it <= ' ' }
+    }
+
+    fun getEmailAddress(): String {
+        return emailAddress.text.toString().trim { it <= ' ' }
+    }
+
+    fun getPassword(): String {
+        return password.text.toString().trim { it <= ' ' }
+    }
+
+    fun moveToDashboardPage(){
+        val dashboardIntent = Intent(this, DashboardPage::class.java)
+        startActivity(dashboardIntent)
+    }
+
+    fun setStatusMessage(message : String){
+        errorText.setText(message.trim())
+    }
 }
